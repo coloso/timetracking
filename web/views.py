14a62230216c381a9588accb6ea815deec4401c4
janosch <janosch.kocsis@gmail.com>
2022-04-18 14:36:10 +0200

    add person crud.

--- web/views.py
+++ web/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Person
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello world.")
+
+
+def persons(request):
+    all_entries = Person.objects.all()
+    context = {'persons': all_entries}
+    return render(request, 'person.html', context)
